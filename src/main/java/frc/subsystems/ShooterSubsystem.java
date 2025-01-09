49ce8dae906772602adb2de2f9f8300d2fb16fb5
BillZhouGH <117119147+BillZhouGH@users.noreply.github.com>
2025-01-09 18:28:50 -0500

    shoot tuah

--- src/main/java/frc/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/subsystems/ShooterSubsystem.java
@@ -1,5 +1,45 @@
 package frc.subsystems;
 
-public class ShooterSubsystem {
-    
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap.ShooterConstants;
+
+public class ShooterSubsystem extends SubsystemBase{
+    private double shootSpeed = 1; // percent of max motor speed -1 to 1
+    private final double wristSpeed = .15; // percent of max motor speed
+    private CANSparkFlex bottomShoot;
+    private CANSparkFlex topShoot;
+    private CANSparkFlex wristControl;
+
+    public ShooterSubsystem() {
+        topShoot = new CANSparkFlex(RobotMap.MotorPorts.INTAKE_MOTOR, MotorType.kBrushless);
+        topShoot.setSmartCurrentLimit(40);
+        topShoot.setInverted(true);
+        topShoot.setIdleMode(IdleMode.kCoast);
+        topShoot.burnFlash();
+
+        bottomShoot = new CANSparkFlex(RobotMap.MotorPorts.INTAKE_MOTOR, MotorType.kBrushless);
+        bottomShoot.setSmartCurrentLimit(40);
+        bottomShoot.setInverted(true);
+        bottomShoot.setIdleMode(IdleMode.kCoast);
+        bottomShoot.burnFlash();
+        
+        wristControl = new CANSparkFlex(RobotMap.MotorPorts.INTAKE_MOTOR, MotorType.kBrushless);
+        wristControl.setSmartCurrentLimit(40);
+        wristControl.setIdleMode(IdleMode.kBrake);
+        wristControl.burnFlash();
+    }
+
+    public shootTuah() {
+        topShoot.set(shootSpeed);
+        bottomShoot.set(shootSpeed);
+    }
+
+    public void stop() {
+        topShoot.set(0);
+        bottomShoot.set(0);
+    }
 }

ff344b8a0a7ec9d18a7a64f3051657405a1f1828
BillZhouGH <117119147+BillZhouGH@users.noreply.github.com>
2025-01-09 18:31:48 -0500

    intake and shoot constants

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,6 +14,51 @@ public class RobotMap {
 
 
 }
+
+public static final class IntakeConstants {
+    public static final double kDistanceWithoutNode = 46.7;
+}
+
+public static final class ShooterConstants {
+        //Shooter Parameters
+
+        public static final double kTopRPM = 3000; //top
+        public static final double kBottomRPM = 3000; //bottom
+        
+        public static final double kTop_P = .1;
+        public static final double kTop_I = 0;//change
+        public static final double kTop_D = 0;
+        public static final double kTop_FF = 1.75;
+        public static final double kTop_MIN_OUTPUT = 0.3;
+        public static final double kTop_MAX_OUTPUT = 1;
+
+        public static final double kBottom_P = .1;
+        public static final double kBottom_I = 0;//change
+        public static final double kBottom_D = 0;
+        public static final double kBottom_FF = 1.75;
+        public static final double kBottom_MIN_OUTPUT = 0.3;
+        public static final double kBottom_MAX_OUTPUT = 1;
+
+        public static final double kWrist_P = 0.055;
+        public static final double kWrist_I = 0.01;
+        public static final double kWrist_D = 0.0;
+
+        public static final double kMinWristAngle = 0.8;
+        public static final double kMaxWristAngle = 0.98;
+
+        public static final double AprilTagHeight = 4.697916667; // height of the center of the speaker apriltag to ground (ft) //h2
+        public static final double LimelightHeight = 26/12; // distance from the center of the limelight lens to ground //h1
+        public static final double TagToSpeakerHeight = 1.896192542; // height from center of apriltag to center of speaker //s
+        public static final double LimelightAngle = 22.5; // how many degrees back is the limelight rotated from perfectly vertical (deg) //a1
+
+        public static final double SpeakerXDistfromCenter = 8.270621; 
+        public static final double SpeakerYDistfromCenter = 1.442212;
+        public static final double XOffset = 0.0254;
+        public static final double YOffset = 0.0889;
+        public static final double dis_LL_to_bumpers = 1.333;// ft
+
+    }
+
 //Motor Ports
 public static final class MotorPorts{
     //MOTORS

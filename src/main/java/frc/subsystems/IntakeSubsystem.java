bd073038dfa73869b88ec943cb486c98acf12d8d
BILL ZHOU <billykzhou@gmail.com>
2025-02-10 19:17:55 -0500

    intake updated

---
+++ src/main/java/frc/subsystems/IntakeSubsystem.java
@@ -0,0 +1,56 @@
+package frc.subsystems;
+
+import java.io.ObjectInputFilter.Config;
+
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+import frc.robot.RobotMap.IntakeConstants;
+
+public class IntakeSubsystem extends SubsystemBase{
+    private final double intakeSpeed = 1; //should be 1
+    private SparkFlex intakeMotor;
+    private AnalogPotentiometer intakeDistanceSensor;
+    private boolean isNoteIn = true;
+
+    public IntakeSubsystem() {
+        // SmartDashboard.putBoolean("Note in Intake", false);
+        intakeMotor = new SparkFlex(RobotMap.MotorPorts.INTAKE_MOTOR, MotorType.kBrushless);
+        // intakeMotor.setSmartCurrentLimit(40);
+        
+        //The type of distance sensor we have in the intake, a 2m rev IR Distance Sensor, must be declared as an analog potentiometer
+        intakeDistanceSensor = new AnalogPotentiometer(0, 100, 30); 
+        // SmartDashboard.putNumber("Intake Distance Sensor", intakeDistanceSensor.get());
+        // SmartDashboard.putBoolean("isNoteIn", isNoteIn);
+    }
+
+    public void runIntake(){
+        intakeMotor.set(-intakeSpeed);
+    }
+
+    public void runOuttake(){
+        intakeMotor.set(.2);
+    }
+
+    public boolean hitSensor(){ 
+        // SmartDashboard.putBoolean("Note in Intake", isNoteIn);
+        return isNoteIn;
+    }
+
+    public void stop(){
+        intakeMotor.set(0);
+    }
+
+    @Override
+    public void periodic(){
+        isNoteIn = intakeDistanceSensor.get() > (IntakeConstants.kDistanceWithoutNode + 5);
+        // SmartDashboard.putNumber("Intake Distance Sensor", intakeDistanceSensor.get());
+        // SmartDashboard.putBoolean("isNoteIn", hitSensor());
+    }
+}
